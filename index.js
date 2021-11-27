da26045ef0d91d857275fe1d9af8f8d579415977
Esteban <luigirp64@gmail.com>
2021-11-26 18:56:00 -0600

    Se valido los campos de inputs, se embelleció el codigo

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-
 function calcular() {
-    const total = document.getElementById("total").value;
-    const hombre = document.getElementById("hombre").value;
-    const mujer = document.getElementById("mujeres").value;
+  const total = document.getElementById("total").value;
+  const hombre = document.getElementById("hombre").value;
+  const mujer = document.getElementById("mujeres").value;
+
+  if (!validar(total, hombre, mujer)) {
+    return;
+  }
 
   const p1 = hombre / total;
   const p2 = mujer / total;
@@ -11,16 +14,32 @@ function calcular() {
   const q2 = 1 - p2;
 
   const largaNegativa =
-   ( p1 - p2) - 1.64 * Math.sqrt(((p1 * q1) / total) + ((p2 * q2) / total));
+    p1 - p2 - 1.64 * Math.sqrt((p1 * q1) / total + (p2 * q2) / total);
   const largaPositiva =
-    (p1 - p2) + 1.64 * Math.sqrt(((p1 * q1) / total) + ((p2 * q2) / total));
-  
-  const redondeo1= Math.round(largaNegativa * 10000) / 10000 ;
-  const redondeo2= Math.round(largaPositiva* 10000) / 10000;
-  document.getElementById("negativo").value=redondeo1;
-  document.getElementById("positivo").value=redondeo2;
+    p1 - p2 + 1.64 * Math.sqrt((p1 * q1) / total + (p2 * q2) / total);
 
-  document.getElementById("negativo2").value=redondeo1;
-  document.getElementById("positivo2").value=redondeo2;
+  const redondeo1 = Math.round(largaNegativa * 10000) / 10000;
+  const redondeo2 = Math.round(largaPositiva * 10000) / 10000;
+  document.getElementById("negativo").value = redondeo1;
+  document.getElementById("positivo").value = redondeo2;
 
+  document.getElementById("negativo2").value = redondeo1;
+  document.getElementById("positivo2").value = redondeo2;
+}
+
+function validar(total, hombre, mujer) {
+  if (!total || !hombre || !mujer) {
+    alert("Uno o varios campos estan vacios");
+    return false;
+  }
+  if (isNaN(total) || isNaN(hombre) || isNaN(mujer)) {
+    alert("Los datos deben ser numericos");
+    return false;
+  }
+  const totalInput = parseInt(hombre) + parseInt(mujer);
+  if (total != totalInput) {
+    alert("La suma de hombres y mujeres deben ser identicas al total");
+    return false;
+  }
+  return true;
 }
